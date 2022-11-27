0e9b2282c13027bdb23c501683e0b184dc4ff9d9
Shaukat Sohail <65353819+naamjee2@users.noreply.github.com>
2022-11-27 21:37:08 +0500

    pointer with array

---
+++ Pointer_Array.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+
+using namespace std;
+int main() {
+   
+
+ int arr[]= {53,12,31,2};
+    
+    cout <<*arr <<endl;      // first element of array;
+    
+    int *pointer =arr;      //53 has been stored in  the pointer 
+    
+    cout << *pointer;       // gives 53
+    
+
+    //suppose that initially the address is 100 
+    // after an iteration it would be incremented by 4 bytes (because int size if 4bytes)
+    // the memory address would be incremented by the 100 + 4 on every iteration  
+
+    for (int i = 1 ; i<=4; i++){
+        cout << *pointer << endl;
+        pointer ++;
+    }
+
+    return 0;
+}
